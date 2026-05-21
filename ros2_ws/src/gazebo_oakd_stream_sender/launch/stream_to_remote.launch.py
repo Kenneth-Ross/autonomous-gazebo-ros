d55ca12093cef87020ae5a85559845986b2be004
Kenneth-Ross <kennethsross20@gmail.com>
2026-05-20 20:41:38 -0700

    fix(streamer): enable use_sim_time support

--- ros2_ws/src/gazebo_oakd_stream_sender/launch/stream_to_remote.launch.py
+++ ros2_ws/src/gazebo_oakd_stream_sender/launch/stream_to_remote.launch.py
@@ -2,13 +2,17 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable
+from launch.actions import SetEnvironmentVariable, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     pkg_share = get_package_share_directory('gazebo_oakd_stream_sender')
     cyclonedds_config = os.path.join(pkg_share, 'config', 'cyclonedds.xml')
 
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
     return LaunchDescription([
+        DeclareLaunchArgument('use_sim_time', default_value='true'),
         SetEnvironmentVariable(name='CYCLONEDDS_URI', value=cyclonedds_config),
         SetEnvironmentVariable(name='RMW_IMPLEMENTATION', value='rmw_cyclonedds_cpp'),
         
@@ -18,6 +22,7 @@ def generate_launch_description():
             package='gazebo_oakd_stream_sender',
             executable='combined_streamer',
             name='oakd_combined_streamer',
+            parameters=[{'use_sim_time': use_sim_time}],
             output='screen'
         ),
         
