aad9daf08e94edddbabe537583c43b669baded83
Kenneth-Ross <kennethsross20@gmail.com>
2026-07-20 21:31:23 -0700

    Revert unsupported CycloneDDS channels

--- server_sim/src/gazebo_oakd_stream_sender/test/test_cyclonedds_contract.py
+++ server_sim/src/gazebo_oakd_stream_sender/test/test_cyclonedds_contract.py
@@ -2,16 +2,12 @@ import pathlib
 import xml.etree.ElementTree as ET
 
 
-def test_camera_channel_paces_new_and_auxiliary_traffic():
+def test_sender_uses_only_supported_jazzy_elements():
     config = pathlib.Path(__file__).parents[1] / 'config' / 'cyclonedds.xml'
     root = ET.parse(config).getroot()
     namespace = {'c': 'https://cdds.io/config'}
-    channel = root.find('./c:Domain/c:Channels/c:Channel', namespace)
 
-    assert channel is not None
-    assert channel.attrib == {'Name': 'camera', 'TransportPriority': '0'}
-    assert channel.findtext('c:DataBandwidthLimit', namespaces=namespace) == '180 Mbps'
-    assert channel.findtext('c:AuxiliaryBandwidthLimit', namespaces=namespace) == '20 Mbps'
+    assert root.find('./c:Domain/c:Channels', namespace) is None
 
 
 def test_sender_has_no_hard_coded_peer():
